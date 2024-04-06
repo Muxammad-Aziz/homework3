94d386e96eedb630350bb39ea1b2e2e8c4c1b83b
Muxammad-Aziz <165257690+Muxammad-Aziz@users.noreply.github.com>
2024-04-06 17:36:51 +0500

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-let name = ("Alex").toLowerCase();
+let name = ("Alex").toLowerCase()
 
 while (true) {
-    name = prompt("Как вас зовут?").toLowerCase();
+    name = prompt("Как вас зовут?").toLowerCase().trim()
     if (name === "alex") {
-        break;
+        break
     } else {
-        alert("Ошибка: Иди гуляй Вася");
+        alert("Ошибка: Иди гуляй Вася")
     }
 }
-console.log("Привет, " + name);
+console.log("Привет, " + name)
 
 
-let accountNumber = 7777;
+let accountNumber = 7777
 
 while (true) {
-    accountNumber = parseFloat(prompt("Введите номер счета:"));
-    if (accountNumber === 7777) {
+    let number = parseFloat(prompt("Введите номер счета:"))
+    if (!isNaN(number) && accountNumber === number) {
         break;
     } else {
-        alert("Ошибка: Неправильный номер счета");
+        alert("Ошибка: Неправильный номер счета")
     }
 }
 console.log("Номер счета, " + accountNumber);
 
-let money = 10000;
+let money = 10000
 
 while (true) {
-    var суммаСнятия = parseFloat(prompt("Введите сумму для снятия:"));
+    var суммаСнятия = parseFloat(prompt("Введите сумму для снятия:"))
     if (!isNaN(суммаСнятия) && суммаСнятия <= money) {
-        money -= суммаСнятия;
-        console.log("Снято: " + суммаСнятия);
-        console.log("Осталось: " + money);
+        money -= суммаСнятия
+        console.log("Снято: " + суммаСнятия)
+        console.log("Осталось: " + money)
         break;
     } else {      
-        console.log("Ошибка: Пашел в ж...!");
+        console.log("Ошибка: Пашел в ж...!")
     }
 }
